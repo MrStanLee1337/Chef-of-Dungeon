2a71a5801e4c22634d02c4a0880884434fc49704
user <fedorshur10@gmail.com>
2025-09-13 21:40:49 +0300

    global deck support

--- scripts/deck.gd
+++ scripts/deck.gd
@@ -6,11 +6,21 @@ extends Node2D
 var _myCards: Array = []
 
 func _ready() -> void:
-	for child in _myCardContainer.get_children():
-		if child is Card:
-			child.set_hidden_in_deck(true)
-			_myCards.append(child)
-			child.get_parent().remove_child(child)
+	var globalDeck = Global.currentDeck
+	var globalCardPath = Global.cardsPath
+	for cardId in globalDeck.keys():
+		var cardScene : PackedScene = load(globalCardPath[cardId])
+		for i in range(globalDeck[cardId]):
+			var cardNode = cardScene.instantiate()
+			cardNode.set_hidden_in_deck(true)
+			#_myCardContainer.add_child(cardNode)
+			_myCards.append(cardNode)
+	_myCards.shuffle()
+	#for child in _myCardContainer.get_children():
+		#if child is Card:
+			#child.set_hidden_in_deck(true)
+			#_myCards.append(child)
+			#child.get_parent().remove_child(child)
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
--- scripts/global.gd
+++ scripts/global.gd
@@ -1,1 +1,15 @@
 extends Node
+
+@export var currentDeck : Dictionary
+@export var cardsPath : Dictionary
+
+func _ready() -> void:
+	currentDeck = {
+		"sample" : 3,
+		"sample2" : 4
+	}
+	
+	cardsPath = {
+		"sample" : "res://scenes/cards/SampleCard.tscn",
+		"sample2" : "res://scenes/cards/SampleCard2.tscn"
+	}
